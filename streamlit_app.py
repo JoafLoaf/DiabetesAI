a71826fd8a3fe0d0cbc1d8663f2bdfcaff8c6036
JoafLoaf <joadkj@gmail.com>
2025-01-16 20:11:48 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,1 @@
-streamlit run "/Users/joad/Desktop/ML model/Diabetes/python code/Diabetes prediction system/diabetes_pred.py"
+python "/Users/joad/Desktop/ML model/Diabetes/python code/Diabetes prediction system/diabetes_pred.py"
