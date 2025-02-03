9cc1fe84b3a102e3669c52bbba03fa2a2adca906
JoafLoaf <joadkj@gmail.com>
2025-02-02 19:23:40 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,1 @@
-my-ai-project/
-app.py              # Your Streamlit app (Main script)
-model.pkl           # Your AI model file (Trained model)
-requirements.txt    # Dependencies for Streamlit Cloud
+
